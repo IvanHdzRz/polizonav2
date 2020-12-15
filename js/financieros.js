bd1cb0e2ad929c850001029c3db6fb31b8579ac6
IvanHdzRZ <musedelick70@gmail.com>
2020-12-15 14:12:24 -0600

    se agrego graficos para los indicadores de mercado

--- js/financieros.js
+++ js/financieros.js
@@ -8,6 +8,7 @@ const fetchData= async()=>{
         //consulta datos desde el servidor
         const req=await fetch("http://polizona.com/mercado/22/php/financieros.php");
         const data=await req.json();
+        //caluclo de indicadores financieros
         const materiaPrimaTotal=data.insumos.reduce((acc,insumo)=>parseInt(insumo.costo_total)+acc,0);
         const capacidadProduccion=Math.min( ...data.insumos.map(insumo=>parseInt (insumo.unidades)/parseInt( insumo.unidades_requeridas_por_producto)));
         const constoProduccionUnitario =data.insumos
@@ -15,9 +16,10 @@ const fetchData= async()=>{
                 parseFloat(insumo.costo_unitario_promedio)*parseFloat(insumo.unidades_requeridas_por_producto)+acc 
             ,0)
 
+        //quita el spinner de loading
         loader.style.display="none";
         
-        //render
+        //render, muestra html en base a calculo de indicadores
         dashboard.innerHTML+=`
             <div class="empresaInfo">
                 <h3>empresa 22</h3>
@@ -32,6 +34,19 @@ const fetchData= async()=>{
                     ${cardFinantial("capacidad de produccion",`${Math.floor (capacidadProduccion)} pzs`,"http://polizona.com/mercado/22/assets/manufacture 1.png")}
                     ${cardFinantial("Costo de materia prima directa por unidad producida",`$ ${Math.round(constoProduccionUnitario)}`,"http://polizona.com/mercado/22/assets/competition 1.png")}
                 </div>
+                <h2 class="sectionTitle">Datos de Mercado</h2>
+                    <div class="cardContainer">
+                        ${cardMarket(
+                            "competidor mas barato",
+                            `${data.costos_de_produccion_competidores[0].id_competidor}`,
+                            "graphComp",
+                            `la empresa ${data.costos_de_produccion_competidores[0].id_competidor} presenta el costo de materia prima mas bajo de toda la ${data.industria[0].nbindustria} `)
+                        }
+                        
+                        
+                    </div>
+                
+
                 <h2 class="sectionTitle">
                     Raw data
                 </h2>
@@ -40,7 +55,8 @@ const fetchData= async()=>{
                 </div>
             </section>
         `;
-
+        
+        //incrustracion de elementos graficos externos (code mirror y graph js)
         const codeEditorContainer=document.getElementById("codeEditor");
         const codeEditor=CodeMirror(codeEditorContainer,{
         
@@ -51,6 +67,17 @@ const fetchData= async()=>{
             styleActiveLine:true,
            
         });
+
+        createGraph(
+            'graphComp',
+            'Costos de produccion competencia',
+            data.costos_de_produccion_competidores.map(comp=>comp.costo_de_produccion),
+            data.costos_de_produccion_competidores.map(comp=>comp.id_competidor)
+        );
+
+        
+        
+
 
     }catch(e){alert}
     
@@ -69,4 +96,45 @@ function cardFinantial (title,value,icon){
         </div>
     `
     return markup;
+}
+
+function cardMarket (title,value,idGrah,comment){
+    const layoutCard=`
+        <div class="cardMarket">
+            <div class="marketResult">
+                <h3>${title}</h3>
+                <h2>${value}</h2>
+                <p>${comment}</p>
+            </div>
+            <div class="graph">
+                
+                <canvas id="${idGrah}"></canvas>
+            </div>
+        </div>
+    
+    `;
+    return layoutCard;
+} 
+
+function createGraph(idGraph,title,xData,yData){
+    const ctx = document.getElementById(idGraph).getContext('2d');
+        const chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'horizontalBar',
+
+            // The data for our dataset
+            data: {
+                labels: yData,
+                datasets: [{
+                    label: title,
+                    backgroundColor: 'rgba(166,68,177,0.5)',
+                    borderColor: '#7444B1',
+                    data: xData,
+                    
+                }]
+            },
+
+            // Configuration options go here
+            options: {}
+        });
 }
